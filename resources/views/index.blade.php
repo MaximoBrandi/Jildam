d80d41f1d89e23155a1e8be6a65bd2c06c6b024e
MaximoBrandi <maximoprandi@gmail.com>
2023-02-05 08:55:32 -0300

    Fourth commit - PUSH TAG 3V78VpUn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,14 @@
 
 @section('bodyClass', 'index')
 
+@section('stylesheets')
+    <link rel="stylesheet" href="assets/vendor/sweetalert2/sweetalert2.min.css">
+@endsection
+
+@section('scripts')
+    <script src="assets/vendor/sweetalert2/sweetalert2.all.min.js"></script>
+@endsection
+
 @section('body')
     <section id="logSec">
         <div id="LogedOut">
@@ -20,6 +28,42 @@
             </form>
         </div>
     </section>
+
+    <script>
+            const swalWithBootstrapButtons = Swal.mixin({
+                customClass: {
+                    confirmButton: 'btn btn-success',
+                    cancelButton: 'btn btn-danger'
+                },
+                buttonsStyling: false
+                })
+
+                swalWithBootstrapButtons.fire({
+                title: 'Aceptas continuar sin ingresar contraseñas reales?',
+                text: "ESTA PAGINA ES UNA DEMO PRUEBA DE UN PROYECTO ANTIGUO, ACTUALMENTE NO ES SEGURO ALMACENAR CONTRASEÑAS REALES AQUI!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Estoy seguro!',
+                cancelButtonText: 'No lo estoy!',
+                reverseButtons: true
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    swalWithBootstrapButtons.fire(
+                    'Aceptaste!',
+                    'Recuerda no ingresar informacion real o confidencial aqui, queda bajo tu responsabilidad.',
+                    'success'
+                    )
+                } else if (
+                    result.dismiss === Swal.DismissReason.cancel
+                ) {
+                    swalWithBootstrapButtons.fire(
+                    'No aceptaste',
+                    'Queda bajo tu responsabiliad si sigues aqui',
+                    'error'
+                    )
+                }
+                })
+    </script>
 @endsection
 
 @section('footerStyle', 'margin-top: 7.5em;')
