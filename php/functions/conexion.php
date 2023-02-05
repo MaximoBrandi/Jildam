84db4a7019cd5fbf1b4bc2a2c8f8ff5d2a9087d6
MaximoBrandi <maximoprandi@gmail.com>
2023-02-05 08:33:07 -0300

    securized conexion.php

--- php/functions/conexion.php
+++ php/functions/conexion.php
@@ -1,8 +1,8 @@
 <?php
-    define('DB_SERVER','localhost');
-    define('DB_USER','root');
+    define('DB_SERVER','');
+    define('DB_USER','');
     define('DB_PASS','');
-    define('DB_NAME','jildam');
+    define('DB_NAME','');
     error_reporting(0);
 
 
